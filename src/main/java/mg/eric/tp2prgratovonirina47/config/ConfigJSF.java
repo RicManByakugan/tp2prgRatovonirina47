c506fb67eb60aa9ff90828af263b3a8fc3903035
Ratovonirina <eric.ratovonirina@gmail.com>
2024-04-06 21:48:16 +0300

    Exercice 1 : analyse d'un post tout simple

---
+++ src/main/java/mg/eric/tp2prgratovonirina47/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.eric.tp2prgratovonirina47.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author Ratovonirina
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
